923269044743c4b48872eacab8b4d9645306bb2c
LiuYang <1005859278@qq.com>
2018-10-25 20:43:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/databasetest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.user.databasetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        dbHelper = new MyDatabaseHelper
+//                (this,"BookStore.db",null,1);
+        //可以让数据库升级只需传入一个大于之前版本号的数字
+        dbHelper = new MyDatabaseHelper
+                (this,"BookStore.db",null,2);
+        Button createDatabase = findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+    }
+}
